216615ae549b42e93808d9c62c19495a96ac4781
NicolasKunzli <nicolas.kunzli@epfl.ch>
2026-02-26 17:10:19 +0100

    Update classData.py

--- classData.py
+++ classData.py
@@ -3,16 +3,19 @@ import numpy as np
 
 class NewSim(SimBarcaForecast):
 
-    def __init__(self):
-        ...
+    def __init__(self, split = "train"):
+        super().__init__(split)
 
     def __len__(self):
-        ...
+        pass
 
     def __getitem__(self, idx):
-        ...
+        return 0
 
     def load_or_compute_metadata(self):
-        ...
+        return None
     
-dataset = NewSim(split="train")
+NewSims = NewSim(split="test")
+
+a = NewSims.get_session_properties()
+print(len(a["session_ids"]))
