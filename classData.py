a47f79c4609fa62674855acee612f29d5b837e80
NicolasKunzli <nicolas.kunzli@epfl.ch>
2026-02-26 17:28:19 +0100

    Update classData.py

--- classData.py
+++ classData.py
@@ -2,20 +2,35 @@ from simbarca_base import SimbarcaBase, SimBarcaForecast
 import numpy as np
 
 class NewSim(SimBarcaForecast):
-
+    """
+    We define the abstract methods needed for the other classes
+    
+    By calling SimBarcaForecast, we also call SimBarcaBase, which also calls BaseDataset
+    """
     def __init__(self, split = "train"):
-        super().__init__(split)
+        super().__init__(split) #super() let us access the parent class, namely SimBarcaForecast
 
     def __len__(self):
+        """
+        PLACEHOLDER
+        """
         pass
 
     def __getitem__(self, idx):
+        """
+        PLACEHOLDER
+        """
         return 0
 
     def load_or_compute_metadata(self):
+        """
+        PLACEHOLDER
+        """
         return None
     
-NewSims = NewSim(split="test")
+NewSims = NewSim(split="train")
 
-a = NewSims.get_session_properties()
-print(len(a["session_ids"]))
+a = NewSims.init_graph_structure()
+print(a)
+b = NewSims.cluster_id
+print(np.shape(b))
